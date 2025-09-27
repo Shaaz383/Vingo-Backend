38c33b6de298b13483ca715ee8f81082c12dfa2b
Shaaz383 <asadakhtar800@gmail.com>
2025-09-27 15:26:17 +0530

    Refactor backend setup: update .gitignore to exclude environment files, enhance index.js to set up an Express server with dotenv configuration, and modify package.json to specify module type.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
