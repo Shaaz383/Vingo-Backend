0bbf1a0e27e79bcb19e9c018adda63fd67688e52
Shaaz383 <asadakhtar800@gmail.com>
2025-09-27 16:20:19 +0530

    Update environment configuration: add MongoDB connection string and JWT secret to .env, enhance .gitignore to exclude .env file, and modify index.js to include database connection on server start.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
+const app = express();
 import dotenv from "dotenv";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-const app = express();
-
+app.use(express.json());
+import connectDB from "./config/db.js";
 app.listen(PORT, () => {
+    connectDB();
     console.log(`Server is running on port ${PORT}`);
 });
