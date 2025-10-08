d09fef7831ae589de9ce6690f2a99cd968a65d07
Shaaz383 <asadakhtar800@gmail.com>
2025-10-08 18:09:59 +0530

    set controller and routes for get my shop

--- controllers/shop.controllers.js
+++ controllers/shop.controllers.js
@@ -27,4 +27,16 @@ export const createEditShop = async (req , res)=>{
     }
 }
 
+export const getMyShop = async  (req , res)=>{
+    try{
+        const shop = await Shop.findOne({owner:req.userId}).populate("owner")
+        if(!shop){
+            return res.status(404).json({message:"Shop not found"})
+        }
+        return res.status(200).json(shop)
+    }
+    catch(error){
+            return res.status(500).json({message:`get my shop error ${error}`})
+    }
+}
 
--- routes/shop.routes.js
+++ routes/shop.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import isAuth from "../middlewares/isAuth.js";
-import { createEditShop } from "../controllers/shop.controllers.js";
+import { createEditShop, getMyShop } from "../controllers/shop.controllers.js";
 const shopRouter = express.Router();
 import {upload} from "../middlewares/multer.js"
 
 
 shopRouter.post("/create-edit",isAuth, upload.single("image"), createEditShop);
+shopRouter.get("/get-my",isAuth, getMyShop);
 
 
 export default shopRouter;
